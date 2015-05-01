e59a6066069d7ab68b6f7cad711e47158ee013a8
quantumtom <cornyn@gmail.com>
2015-05-01 11:09:17 -0700

    Changes to Grunt watch task, CSS, and markup.

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,7 +47,7 @@ module.exports = function (grunt) {
         watch: {
             dev: {
                 files: ['src/**/*.html', 'src/**/*.css', 'src/**/*.js'],
-                tasks: ['ccjs']
+                tasks: ['default']
             }
         },
         clean: {
