9be23a344cb71687e0a909341e5590893e4041f5
quantumtom <cornyn@gmail.com>
2015-07-18 14:46:02 -0700

    Lots more cleanup. Much more readable now.

--- src/js/app.js
+++ src/js/app.js
@@ -32,87 +32,17 @@
             APP.loadFrames();
         });
 
+        APP.util = new APP.utilities();
+
     };
 
-    /**
-     * Injects image DOM nodes. It calculates the URI for each image
-     * used as frames in the imagination to use as values for the src
-     * attributes in each image element on the page.
-     *
-     * Each satellite generates an image every thirty minutes. The second satellite timing is
-     * offset fifteen minutes from the first.
-     *
-     * E.G.
-     *
-     * GOES-N timing        1200Z    |   1230Z    |   1300Z    |   1330Z
-     * GOES-P timing        1215Z    |   1245Z    |   1315Z    |   1345Z
-     *
-     */
-
-    APP.loadFrames = function() {
-
-        function getEnhancement() {
-            var theEnhancementList = APP.theEnhancementList;
-
-            return theEnhancementList[theEnhancementList.selectedIndex].value;
-        }
-
-        function getThePast() {
-            var thePresent = new Date();
-            var theHours = thePresent.getHours();
-
-            thePresent.setHours(theHours - 4);
-
-            return thePresent;
-        }
-
-        /**
-         * Takes a value between 0 - 59 and returns either the
-         * bottom or top half of the hour.
-         * @param m
-         * @param p
-         * @returns {number}
-         */
-        function parseMinutes(m, p) {
-
-            m = m / p;
-            m = Math.floor(m);
-
-            return m * p;
-
-        }
-
-        /**
-         * Takes a places count in Base-10 (ones, tens, hundreds) and prepends
-         * it (concatenates it with a numeric string) that is
-         * passed in as the first parameter.
-         * @param i
-         * @param places
-         * @returns {string}
-         */
-        function padZeroes(i, places) {
-
-            var j;
-            var theString = "";
-
-            theString += i;
-
-            for (j = 0; j < places; j = j + 1) {
-                if (theString.length < places) {
-                    theString = "0" + theString;
-                }
-            }
-
-            return theString;
-
-        }
-
+    APP.utilities = function () {
         /**
          * Takes a date and calculates how many days into the year that date is.
          * @param myDate
          * @returns {number}
          */
-        function dayOfYear(myDate) {
+        this.dayOfYear = function(myDate) {
 
             var theMonth = myDate.getMonth(),
                 theDate = myDate.getDate(),
@@ -134,7 +64,84 @@
             theDayOfTheYear += theDate;
 
             return theDayOfTheYear;
-        }
+        };
+
+        /**
+         * Takes a value between 0 - 59 and returns either the
+         * bottom or top half of the hour.
+         * @param m
+         * @param p
+         * @returns {number}
+         */
+        this.parseMinutes = function(m, p) {
+
+            m = m / p;
+            m = Math.floor(m);
+
+            return m * p;
+
+        };
+
+        /**
+         * Takes a places count in Base-10 (ones, tens, hundreds) and prepends
+         * it (concatenates it with a numeric string) that is
+         * passed in as the first parameter.
+         * @param i
+         * @param places
+         * @returns {string}
+         */
+        this.padZeroes = function(i, places) {
+
+            var j;
+            var theString = "";
+
+            theString += i;
+
+            for (j = 0; j < places; j = j + 1) {
+                if (theString.length < places) {
+                    theString = "0" + theString;
+                }
+            }
+
+            return theString;
+
+        };
+
+        this.getEnhancement = function() {
+            var theEnhancementList = APP.theEnhancementList;
+
+            return theEnhancementList[theEnhancementList.selectedIndex].value;
+        };
+
+        this.getThePast = function() {
+            var thePresent = new Date();
+            var theHours = thePresent.getHours();
+
+            thePresent.setHours(theHours - 4);
+
+            return thePresent;
+        };
+
+    };
+
+    /**
+     * Injects image DOM nodes. It calculates the URI for each image
+     * used as frames in the imagination to use as values for the src
+     * attributes in each image element on the page.
+     *
+     * Each satellite generates an image every thirty minutes. The second satellite timing is
+     * offset fifteen minutes from the first.
+     *
+     * E.G.
+     *
+     * GOES-N timing        1200Z    |   1230Z    |   1300Z    |   1330Z
+     * GOES-P timing        1215Z    |   1245Z    |   1315Z    |   1345Z
+     *
+     */
+
+    APP.loadFrames = function() {
+
+        var util = APP.util;
 
         /**
          * This function dynamically generates the NOAA-specific file path for the remote
@@ -147,13 +154,13 @@
             var theYear = thePast.getFullYear(),
                 theMinutes = thePast.getMinutes(),
                 theHours = thePast.getHours(),
-                theDays = dayOfYear(thePast),
+                theDays = util.dayOfYear(thePast),
                 baseURI = "http://www.ssd.noaa.gov/" + APP.theMapList[APP.theMapList.selectedIndex].value + "/img/",
                 thePeriod = 30,
                 theOffset = 0;
 
-            theDays = padZeroes(theDays, 3);
-            theHours = padZeroes(theHours, 2);
+            theDays = util.padZeroes(theDays, 3);
+            theHours = util.padZeroes(theHours, 2);
 
             /**
              * Handle differences in the timing between the satellite
@@ -174,15 +181,15 @@
                 theOffset = 15;
             }
 
-            theMinutes = parseMinutes(theMinutes, thePeriod);
+            theMinutes = util.parseMinutes(theMinutes, thePeriod);
 
             /** Add the offfset. */
             theMinutes = theMinutes + theOffset;
 
             /** Add a leading zero if the _minutes_ value is a single-digit. */
-            theMinutes = padZeroes(theMinutes, 2);
+            theMinutes = util.padZeroes(theMinutes, 2);
 
-            return baseURI + theYear + theDays + "_" + theHours + theMinutes + getEnhancement() + ".jpg";
+            return baseURI + theYear + theDays + "_" + theHours + theMinutes + util.getEnhancement() + ".jpg";
         }
 
         /** Remove any existing animation frames (images). */
@@ -190,7 +197,7 @@
             APP.panelRoot.removeChild(APP.panelRoot.firstChild);
         }
 
-        var thePast = getThePast();
+        var thePast = util.getThePast();
         var frameNumber;
         var frameElement;
 
