7156cf62617e228f2437edff0fb9029ac0beb4da
quantumtom <cornyn@gmail.com>
2015-09-02 16:46:19 -0700

    Rebuilt distribution.

---
+++ build/js/src/js/wopr.js
@@ -0,0 +1,160 @@
+goog.provide('WOPR.main');
+goog.require('WOPR.helpers');
+
+/**
+ * @license MIT
+ *
+ * @module WOPR.main
+ *
+ * Each satellite generates an image every thirty minutes. The second satellite timing is
+ * offset fifteen minutes from the first.
+ *
+ * Example:
+ *
+ * GOES-N timing        1200Z    |   1230Z    |   1300Z    |   1330Z
+ * GOES-P timing        1215Z    |   1245Z    |   1315Z    |   1345Z
+ */
+
+(function() {
+
+    'use strict';
+
+    var WOPR = function() {
+        WOPR.init();
+        WOPR.loadPage();
+        WOPR.animate(0);
+    };
+
+    WOPR.controls = {
+        mapList: document.getElementById('mapList'),
+        enhancementList: document.getElementById('enhancementList')
+    };
+
+    WOPR.init = function() {
+        for (var i in WOPR.controls) {
+            WOPR.controls[i].addEventListener('change', function() {
+                WOPR.loadPage();
+            });
+        }
+
+        this.display = document.getElementById('display');
+        this.frames = document.getElementsByClassName('frame');
+        
+        this.animation = {
+            interval: 160
+        };
+    };
+
+    WOPR.loadPage = function() {
+        WOPR.theMap = WOPR.controls.mapList[WOPR.controls.mapList.selectedIndex].value;
+        WOPR.theEnhancement = WOPR.controls.enhancementList[WOPR.controls.enhancementList.selectedIndex].value;
+
+        /**
+         * Create an array of image elements into the DOM.
+         */
+
+        var fCount;
+        var fElement;
+        var fArray = WOPR.makeFrames();
+
+        while (WOPR.display.firstChild) {
+            WOPR.display.removeChild(WOPR.display.firstChild);
+        }
+
+        /** Build image tags for each frame of the animated loop. Then inject each into the DOM. */
+        for (fCount = 0; fCount < fArray.length; fCount++) {
+
+            fElement = document.createElement('img');
+
+            fElement.src = fArray[fCount];
+
+            fElement.setAttribute('alt', 'Image #' + (fCount + 1).padZeroes(2));
+            fElement.classList.add('frame');
+
+            WOPR.display.appendChild(fElement);
+        }
+    };
+
+    /**
+     * This function dynamically generates the NOAA-specific file path for the remote
+     * image resources.
+     * @param {Date} thePast
+     * @returns {string}
+     */
+
+    WOPR.makeURI = function(thePast) {
+
+        var baseURI = 'http://www.ssd.noaa.gov/' + WOPR.theMap + '/img/';
+        var theYear = thePast.getFullYear();
+        var theDay = thePast.getDayOfYear();
+        var theHour = thePast.getFormattedHour();
+        var theMinute = thePast.getFormattedMinute();
+        var timeStamp = theYear + theDay + '_' + theHour;
+
+        /** GOES-East: 15 minute offset from GOES-West */
+        if (WOPR.theMap.search('east') >= 0) {
+            theMinute = theMinute + 15;
+        }
+
+        /** Add a leading zero if the _minutes_ value is for the top of the hour. */
+        if (theMinute) {
+            theMinute.padZeroes(2);
+        } else {
+            theMinute = '00';
+        }
+
+        timeStamp = timeStamp + theMinute;
+
+        return baseURI + timeStamp + WOPR.theEnhancement + '.jpg';
+    };
+
+    WOPR.makeFrames = function() {
+        var startTime = new Date();
+
+        startTime.setUTCHours(startTime.getUTCHours() - 1);
+
+        var tempArray = [];
+
+        for (var i = 0; i < 15; i = i + 1) {
+            tempArray.push(WOPR.makeURI(startTime));
+            startTime.setUTCMinutes(startTime.getUTCMinutes() + 30);
+        }
+
+        return tempArray;
+    };
+
+    /**
+     * Animates the individual image frames.
+     * @param {number} n
+     */
+
+    WOPR.animate = function(n) {
+
+        var theFrames = WOPR.frames;
+
+        if (!n) {
+            n = 0;
+        }
+
+        setTimeout(function() {
+
+            theFrames.item(n).classList.add('hidden');
+
+            if (n < theFrames.length - 1) {
+                n = n + 1;
+            } else {
+                n = 0;
+            }
+
+            theFrames.item(n).classList.remove('hidden');
+
+            WOPR.animate(n);
+
+
+        }, WOPR.animation.interval);
+
+    };
+
+    WOPR.call();
+
+}());
--- build/js/wopr.js
+++ build/js/wopr.js
@@ -1,7 +1,15 @@
 (function(){/*
  MIT
 
- @module WOPR
+ @module WOPR.helpers
+
+ A library of a few little helper functions.
+
+*/
+Date.prototype.a=function(){var a=this.getMonth(),b=this.getDate(),c=[31,29,31,30,31,30,31,31,30,31,30,31],d=0,e;this.getYear()%4&&(c[1]=28);for(e=0;e<a;e+=1)d+=c[e];return d=(d+b).f(3)};Date.prototype.b=function(){var a=this.getHours();return a=a.f(2)};Date.prototype.c=function(){return this.getMinutes().u()};Number.prototype.u=function(){var a;a=Math.floor(this/30);return a*=30};Number.prototype.f=function(a){for(var b=this.toString();b.length<a;)b="0"+b;return b};/*
+ MIT
+
+ @module WOPR.main
 
  Each satellite generates an image every thirty minutes. The second satellite timing is
  offset fifteen minutes from the first.
@@ -11,8 +19,7 @@
  GOES-N timing        1200Z    |   1230Z    |   1300Z    |   1330Z
  GOES-P timing        1215Z    |   1245Z    |   1315Z    |   1345Z
 */
-(function(){function a(){a.a=document.getElementById("viewerPanel");a.f=document.getElementById("theMapList");a.c=document.getElementById("theEnhancementList");a.j();a.b();a.h(0);a.f.addEventListener("change",function(){a.b()});a.c.addEventListener("change",function(){a.b()})}a.b=function(){a.i=a.f[a.f.selectedIndex].value;a.v=a.c[a.c.selectedIndex].value;a.l();a.m()};a.j=function(){Date.prototype.a=function(){var b=this.getMonth(),a=this.getDate(),d=[31,29,31,30,31,30,31,31,30,31,30,31],e=0,f;this.getYear()%
-4&&(d[1]=28);for(f=0;f<b;f+=1)e+=d[f];return e=(e+a).g(3)};Date.prototype.b=function(){var b=this.getHours();return b=b.g(2)};Date.prototype.c=function(){return this.getMinutes().u()};Number.prototype.u=function(){var b;b=Math.floor(this/30);return b*=30};Number.prototype.g=function(b){for(var a=this.toString();a.length<b;)a="0"+a;return a}};a.s=function(b){var c="http://www.ssd.noaa.gov/"+a.i+"/img/",d=b.getFullYear(),e=b.a(),f=b.b();b=b.c();0<=a.i.search("east")&&(b+=15);0===b&&(b="00");return c+
-(d+e+"_"+f+b)+a.v+".jpg"};a.o=function(){var b=new Date;b.setUTCHours(b.getUTCHours()-1);var c,d=[];for(c=0;15>c;c+=1)d.push(a.s(b)),b.setMinutes(b.getMinutes()+30);return d};a.l=function(){for(;a.a.firstChild;)a.a.removeChild(a.a.firstChild)};a.m=function(){var b,c,d=a.o(),e;for(b=0;b<d.length;b++)e="image_"+b,c=document.createElement("img"),c.src=d[b],c.id=e,c.setAttribute("alt","Image #"+(b+1).g(2)),c.classList.add("NOAA_JPEG"),c.classList.add("hidden"),c.classList.add("frame"),a.a.appendChild(c)};
-a.h=function(b){var c=document.getElementsByClassName("frame");b=b||0;setTimeout(function(){c.item(b).classList.add("hidden");b=b<c.length-1?b+1:0;c.item(b).classList.remove("hidden");a.h(b)},160)};a.call()})();}).call(window);
+(function(){function a(){a.l();a.c();a.b(0)}a.a={i:document.getElementById("mapList"),g:document.getElementById("enhancementList")};a.l=function(){for(var b in a.a)a.a[b].addEventListener("change",function(){a.c()});this.display=document.getElementById("display");this.frames=document.getElementsByClassName("frame");this.j={interval:160}};a.c=function(){a.h=a.a.i[a.a.i.selectedIndex].value;a.s=a.a.g[a.a.g.selectedIndex].value;for(var b,c,d=a.m();a.display.firstChild;)a.display.removeChild(a.display.firstChild);
+for(b=0;b<d.length;b++)c=document.createElement("img"),c.src=d[b],c.setAttribute("alt","Image #"+(b+1).f(2)),c.classList.add("frame"),a.display.appendChild(c)};a.o=function(b){var c="http://www.ssd.noaa.gov/"+a.h+"/img/",d=b.c();b=b.getFullYear()+b.a()+"_"+b.b();0<=a.h.search("east")&&(d+=15);d||(d="00");return c+(b+d)+a.s+".jpg"};a.m=function(){var b=new Date;b.setUTCHours(b.getUTCHours()-1);for(var c=[],d=0;15>d;d+=1)c.push(a.o(b)),b.setUTCMinutes(b.getUTCMinutes()+30);return c};a.b=function(b){var c=
+a.frames;b||(b=0);setTimeout(function(){c.item(b).classList.add("hidden");b=b<c.length-1?b+1:0;c.item(b).classList.remove("hidden");a.b(b)},a.j.interval)};a.call()})();}).call(window);
 //# sourceMappingURL=wopr.js.map.json
