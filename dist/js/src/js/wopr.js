f6fed84caa5ec09365c38f03f3c0e64197b5ec39
quantumtom <cornyn@gmail.com>
2015-09-04 13:32:16 -0700

    More (maybe too much) refactor.

--- dist/js/src/js/wopr.js
+++ dist/js/src/js/wopr.js
@@ -26,142 +26,132 @@ goog.require('WOPR.helpers');
         WOPR.animateFrames(0);
     };
 
-    WOPR.controls = {
-        mapList: document.getElementById('mapList'),
-        enhancementList: document.getElementById('enhancementList')
-    };
-
     WOPR.init = function() {
-        WOPR.interval = 160;
-        WOPR.display = document.getElementById('display');
-    };
+        this.interval = 160;
+        this.display = document.getElementById('display');
 
-    WOPR.getEnhancement = function() {
-        return WOPR.controls.enhancementList[WOPR.controls.enhancementList.selectedIndex].value;
-    };
+        this.controls = {
+            regionList: document.getElementById('regionList'),
+            enhancementList: document.getElementById('enhancementList')
+        };
 
-    WOPR.getMap = function() {
-        return WOPR.controls.mapList[WOPR.controls.mapList.selectedIndex].value;
+        this.getEnhancement = function() {
+            return this.controls.enhancementList[this.controls.enhancementList.selectedIndex].value;
+        };
+
+        this.getMap = function() {
+            return this.controls.regionList[this.controls.regionList.selectedIndex].value;
+        };
+
+        this.setStartTime = function(newTime) {
+            this.startTime = newTime;
+        };
+
+        /**
+         * Dynamically generates the NOAA-specific file path for the remote image resources.
+         * @returns {string}
+         */
+
+        this.getURI = function() {
+            return _getPath() + _getFileName();
+        };
+
+        function _getPath() {
+            return 'http://www.ssd.noaa.gov/' + WOPR.getMap() + '/img/';
+        }
+
+        function _getFileName() {
+            return _getTimeStamp() + WOPR.getEnhancement() + '.jpg';
+        }
+
+        function _getTimeStamp() {
+            function _adjustForSatellite(theMinute) {
+                var theMap = WOPR.getMap();
+
+                /** GOES-East: 15 minute offset from GOES-West */
+                if (theMap.search('east') > 0) {
+                    return theMinute + 15;
+                } else {
+                    return theMinute;
+                }
+            }
+
+            function _adjustForZeroMinutes(theMinute) {
+                /** Add a leading zero if the _minutes_ value is for the top of the hour. */
+                if (theMinute === 0) {
+                    return '00';
+                } else {
+                    return theMinute.padZeroes(2);
+                }
+            }
+
+            var theMinute = WOPR.startTime.getFormattedMinute();
+
+            theMinute = _adjustForSatellite(theMinute);
+            theMinute = _adjustForZeroMinutes(theMinute);
+
+            return WOPR.startTime.getFullYear() + WOPR.startTime.getDayOfYear() +
+                '_' + WOPR.startTime.getFormattedHour() + theMinute;
+        }
+
+        this.getFrames = function() {
+            var tempArray = [];
+            var newTime;
+
+            newTime = WOPR.startTime;
+
+            for (var i = 0; i < 15; i = i + 1) {
+                tempArray.push(WOPR.getURI());
+                newTime.setUTCMinutes(newTime.getUTCMinutes() + 30);
+                WOPR.setStartTime(newTime);
+            }
+
+            return tempArray;
+        };
+
+
     };
 
     WOPR.loadPage = function() {
-        WOPR.resetStartTime();
-        WOPR.wireEvents();
-        WOPR.loadImages();
-    };
-
-    WOPR.resetStartTime = function() {
         var startTime = new Date();
 
         startTime.setUTCHours(startTime.getUTCHours() - 1);
 
         WOPR.setStartTime(startTime);
-    };
 
-    WOPR.setStartTime = function(newTime) {
-        WOPR.startTime = newTime;
-    };
-
-    WOPR.wireEvents = function() {
         for (var item in WOPR.controls) {
             WOPR.controls[item].addEventListener('change', function() {
                 WOPR.loadPage();
             });
         }
+
+        WOPR.loadImages();
     };
 
     /**
      * Create an array of image elements into the DOM.
      */
-
     WOPR.loadImages = function() {
         var imgElement;
-        var fArray = WOPR.buildFrames();
+        var fArray = WOPR.getFrames();
 
-        WOPR.clearImages();
+        /** Remove any images that may have been loaded previously. */
+        while (WOPR.display.firstChild) {
+            WOPR.display.removeChild(WOPR.display.firstChild);
+        }
 
         /** Build image tags for each frame of the animated loop. Then inject each into the DOM. */
-        for (var fCount = 0; fCount < fArray.length; fCount++) {
+        for (var c = 0; c < fArray.length; c++) {
 
             imgElement = document.createElement('img');
 
-            imgElement.src = fArray[fCount];
+            imgElement.src = fArray[c];
 
-            imgElement.setAttribute('alt', 'Image #' + (fCount + 1).padZeroes(2));
+            imgElement.setAttribute('alt', 'Image #' + (c + 1).padZeroes(2));
             imgElement.classList.add('frame');
             imgElement.classList.add('hidden');
 
             WOPR.display.appendChild(imgElement);
-        }
-    };
-
-    WOPR.clearImages = function() {
-        while (WOPR.display.firstChild) {
-            WOPR.display.removeChild(WOPR.display.firstChild);
-        }
-    };
-
-    WOPR.buildFrames = function() {
-        var i;
-        var tempArray = [];
-        var newTime;
-
-        newTime = WOPR.startTime;
-
-        for (i = 0; i < 15; i = i + 1) {
-            tempArray.push(WOPR.getURI());
-            newTime.setUTCMinutes(newTime.getUTCMinutes() + 30);
-            WOPR.setStartTime(newTime);
-        }
-
-        return tempArray;
-    };
-
-    /**
-     * This function dynamically generates the NOAA-specific file path for the remote
-     * image resources.
-     * @returns {string}
-     */
-
-    WOPR.getURI = function() {
-        return WOPR.getPath() + WOPR.getFileName();
-    };
-
-    WOPR.getPath = function() {
-        return 'http://www.ssd.noaa.gov/' + WOPR.getMap() + '/img/';
-    };
-
-    WOPR.getFileName = function() {
-        return WOPR.getTimeStamp() + WOPR.getEnhancement() + '.jpg';
-    };
-
-    WOPR.getTimeStamp = function() {
-        var theMinute = WOPR.startTime.getFormattedMinute();
-
-        theMinute = WOPR.adjustForSatellite(theMinute);
-        theMinute = WOPR.adjustForZeroMinutes(theMinute);
-
-        return WOPR.startTime.getFullYear() + WOPR.startTime.getDayOfYear() +
-            '_' + WOPR.startTime.getFormattedHour() + theMinute;
-    };
-
-    WOPR.adjustForSatellite = function(theMinute) {
-        var theMap = WOPR.getMap();
-
-        /** GOES-East: 15 minute offset from GOES-West */
-        if (theMap.search('east') > 0) {
-            return theMinute + 15;
-        } else {
-            return theMinute;
-        }
-    };
-
-    WOPR.adjustForZeroMinutes = function(theMinute) {
-        /** Add a leading zero if the _minutes_ value is for the top of the hour. */
-        if (theMinute === 0) {
-            return '00';
-        } else {
-            return theMinute.padZeroes(2);
         }
     };
 
