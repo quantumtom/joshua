cf41ccab9e83a3995f8cce9845aab97e6a0f8909
quantumtom <cornyn@gmail.com>
2015-09-04 11:48:43 -0700

    Rebuilding distribution directory.

--- dist/js/src/js/wopr.js
+++ dist/js/src/js/wopr.js
@@ -23,7 +23,7 @@ goog.require('WOPR.helpers');
     var WOPR = function() {
         WOPR.init();
         WOPR.loadPage();
-        WOPR.animate(0);
+        WOPR.animateFrames(0);
     };
 
     WOPR.controls = {
@@ -32,94 +32,137 @@ goog.require('WOPR.helpers');
     };
 
     WOPR.init = function() {
-        for (var i in WOPR.controls) {
-            WOPR.controls[i].addEventListener('change', function() {
-                WOPR.loadPage();
-            });
-        }
-
         WOPR.interval = 160;
         WOPR.display = document.getElementById('display');
     };
 
+    WOPR.getEnhancement = function() {
+        return WOPR.controls.enhancementList[WOPR.controls.enhancementList.selectedIndex].value;
+    };
+
+    WOPR.getMap = function() {
+        return WOPR.controls.mapList[WOPR.controls.mapList.selectedIndex].value;
+    };
+
     WOPR.loadPage = function() {
-        WOPR.theMap = WOPR.controls.mapList[WOPR.controls.mapList.selectedIndex].value;
-        WOPR.theEnhancement = WOPR.controls.enhancementList[WOPR.controls.enhancementList.selectedIndex].value;
+        WOPR.resetStartTime();
+        WOPR.wireEvents();
+        WOPR.loadImages();
+    };
 
-        /**
-         * Create an array of image elements into the DOM.
-         */
+    WOPR.resetStartTime = function() {
+        var startTime = new Date();
 
-        var fCount;
-        var fElement;
-        var fArray = WOPR.makeFrames();
+        startTime.setUTCHours(startTime.getUTCHours() - 1);
 
+        WOPR.setStartTime(startTime);
+    };
+
+    WOPR.setStartTime = function(newTime) {
+        WOPR.startTime = newTime;
+    };
+
+    WOPR.wireEvents = function() {
+        for (var item in WOPR.controls) {
+            WOPR.controls[item].addEventListener('change', function() {
+                WOPR.loadPage();
+            });
+        }
+    };
+
+    /**
+     * Create an array of image elements into the DOM.
+     */
+
+    WOPR.loadImages = function() {
+        var imgElement;
+        var fArray = WOPR.buildFrames();
+
+        WOPR.clearImages();
+
+        /** Build image tags for each frame of the animated loop. Then inject each into the DOM. */
+        for (var fCount = 0; fCount < fArray.length; fCount++) {
+
+            imgElement = document.createElement('img');
+
+            imgElement.src = fArray[fCount];
+
+            imgElement.setAttribute('alt', 'Image #' + (fCount + 1).padZeroes(2));
+            imgElement.classList.add('frame');
+            imgElement.classList.add('hidden');
+
+            WOPR.display.appendChild(imgElement);
+        }
+    };
+
+    WOPR.clearImages = function() {
         while (WOPR.display.firstChild) {
             WOPR.display.removeChild(WOPR.display.firstChild);
         }
+    };
 
-        /** Build image tags for each frame of the animated loop. Then inject each into the DOM. */
-        for (fCount = 0; fCount < fArray.length; fCount++) {
+    WOPR.buildFrames = function() {
+        var i;
+        var tempArray = [];
+        var newTime;
 
-            fElement = document.createElement('img');
+        newTime = WOPR.startTime;
 
-            fElement.src = fArray[fCount];
-
-            fElement.setAttribute('alt', 'Image #' + (fCount + 1).padZeroes(2));
-            fElement.classList.add('frame');
-            fElement.classList.add('hidden');
-
-            WOPR.display.appendChild(fElement);
+        for (i = 0; i < 15; i = i + 1) {
+            tempArray.push(WOPR.getURI());
+            newTime.setUTCMinutes(newTime.getUTCMinutes() + 30);
+            WOPR.setStartTime(newTime);
         }
+
+        return tempArray;
     };
 
     /**
      * This function dynamically generates the NOAA-specific file path for the remote
      * image resources.
-     * @param {Date} thePast
      * @returns {string}
      */
 
-    WOPR.makeURI = function(thePast) {
-
-        var baseURI = 'http://www.ssd.noaa.gov/' + WOPR.theMap + '/img/';
-        var theYear = thePast.getFullYear();
-        var theDay = thePast.getDayOfYear();
-        var theHour = thePast.getFormattedHour();
-        var theMinute = thePast.getFormattedMinute();
-        var timeStamp = theYear + theDay + '_' + theHour;
-
-        /** GOES-East: 15 minute offset from GOES-West */
-        if (WOPR.theMap.search('east') >= 0) {
-            theMinute = theMinute + 15;
-        }
-
-        /** Add a leading zero if the _minutes_ value is for the top of the hour. */
-        if (theMinute === 0) {
-            theMinute = '00';
-        } else {
-            theMinute.padZeroes(2);
-        }
-
-        timeStamp = timeStamp + theMinute;
-
-        return baseURI + timeStamp + WOPR.theEnhancement + '.jpg';
+    WOPR.getURI = function() {
+        return WOPR.getPath() + WOPR.getFileName();
     };
 
-    WOPR.makeFrames = function() {
-        var startTime = new Date();
+    WOPR.getPath = function() {
+        return 'http://www.ssd.noaa.gov/' + WOPR.getMap() + '/img/';
+    };
 
-        startTime.setUTCHours(startTime.getUTCHours() - 1);
+    WOPR.getFileName = function() {
+        return WOPR.getTimeStamp() + WOPR.getEnhancement() + '.jpg';
+    };
 
-        var i;
-        var tempArray = [];
+    WOPR.getTimeStamp = function() {
+        var theMinute = WOPR.startTime.getFormattedMinute();
 
-        for (i = 0; i < 15; i = i + 1) {
-            tempArray.push(WOPR.makeURI(startTime));
-            startTime.setUTCMinutes(startTime.getUTCMinutes() + 30);
+        theMinute = WOPR.adjustForSatellite(theMinute);
+        theMinute = WOPR.adjustForZeroMinutes(theMinute);
+
+        return WOPR.startTime.getFullYear() + WOPR.startTime.getDayOfYear() +
+            '_' + WOPR.startTime.getFormattedHour() + theMinute;
+    };
+
+    WOPR.adjustForSatellite = function(theMinute) {
+        var theMap = WOPR.getMap();
+
+        /** GOES-East: 15 minute offset from GOES-West */
+        if (theMap.search('east') > 0) {
+            return theMinute + 15;
+        } else {
+            return theMinute;
         }
+    };
 
-        return tempArray;
+    WOPR.adjustForZeroMinutes = function(theMinute) {
+        /** Add a leading zero if the _minutes_ value is for the top of the hour. */
+        if (theMinute === 0) {
+            return '00';
+        } else {
+            return theMinute.padZeroes(2);
+        }
     };
 
     /**
@@ -127,7 +170,7 @@ goog.require('WOPR.helpers');
      * @param {number} n
      */
 
-    WOPR.animate = function(n) {
+    WOPR.animateFrames = function(n) {
 
         var theFrames = document.getElementsByClassName('frame');
 
@@ -147,7 +190,7 @@ goog.require('WOPR.helpers');
 
             theFrames.item(n).classList.remove('hidden');
 
-            WOPR.animate(n);
+            WOPR.animateFrames(n);
 
 
         }, WOPR.interval);
