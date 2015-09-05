002a6d63e6c983f6de032b57191ad1733e427009
quantumtom <cornyn@gmail.com>
2015-09-05 14:16:07 -0700

    Fully linted against jsHint.

--- src/js/wopr.js
+++ src/js/wopr.js
@@ -16,17 +16,17 @@ goog.require('WOPR.helpers');
  * GOES-P timing        1215Z    |   1245Z    |   1315Z    |   1345Z
  */
 
-(function() {
+(function () {
 
     'use strict';
 
-    var WOPR = function() {
+    var WOPR = function () {
         WOPR.init();
         WOPR.loadPage();
         WOPR.animateFrames(0);
     };
 
-    WOPR.init = function() {
+    WOPR.init = function () {
         this.interval = 160;
         this.display = document.getElementById('display');
 
@@ -35,72 +35,73 @@ goog.require('WOPR.helpers');
             enhancementList: document.getElementById('enhancementList')
         };
 
-        this.getEnhancement = function() {
+        this.getEnhancement = function () {
             return this.controls.enhancementList[this.controls.enhancementList.selectedIndex].value;
         };
 
-        this.getMap = function() {
+        this.getMap = function () {
             return this.controls.regionList[this.controls.regionList.selectedIndex].value;
         };
 
-        this.setStartTime = function(newTime) {
+        this.setStartTime = function (newTime) {
             this.startTime = newTime;
         };
+
+        function getPath() {
+            return 'http://www.ssd.noaa.gov/' + WOPR.getMap() + '/img/';
+        }
+
+        function getTimeStamp() {
+            function adjustForSatellite(theMinute) {
+                var theMap = WOPR.getMap();
+
+                /** GOES-East: 15 minute offset from GOES-West */
+                if (theMap.search('east') > 0) {
+                    theMinute = theMinute + 15;
+                }
+
+                return theMinute;
+            }
+
+            function adjustForZeroMinutes(theMinute) {
+                /** Add a leading zero if the _minutes_ value is for the top of the hour. */
+                if (theMinute === 0) {
+                    theMinute = '00';
+                } else {
+                    theMinute.padZeroes(2);
+                }
+
+                return theMinute;
+            }
+
+            var theMinute = WOPR.startTime.getFormattedMinute();
+
+            theMinute = adjustForSatellite(theMinute);
+            theMinute = adjustForZeroMinutes(theMinute);
+
+            return WOPR.startTime.getFullYear() + WOPR.startTime.getDayOfYear() +
+                '_' + WOPR.startTime.getFormattedHour() + theMinute;
+        }
+
+        function getFileName() {
+            return getTimeStamp() + WOPR.getEnhancement() + '.jpg';
+        }
 
         /**
          * Dynamically generates the NOAA-specific file path for the remote image resources.
          * @returns {string}
          */
 
-        this.getURI = function() {
-            return _getPath() + _getFileName();
+        this.getURI = function () {
+            return getPath() + getFileName();
         };
 
-        function _getPath() {
-            return 'http://www.ssd.noaa.gov/' + WOPR.getMap() + '/img/';
-        }
-
-        function _getFileName() {
-            return _getTimeStamp() + WOPR.getEnhancement() + '.jpg';
-        }
-
-        function _getTimeStamp() {
-            function _adjustForSatellite(theMinute) {
-                var theMap = WOPR.getMap();
-
-                /** GOES-East: 15 minute offset from GOES-West */
-                if (theMap.search('east') > 0) {
-                    return theMinute + 15;
-                } else {
-                    return theMinute;
-                }
-            }
-
-            function _adjustForZeroMinutes(theMinute) {
-                /** Add a leading zero if the _minutes_ value is for the top of the hour. */
-                if (theMinute === 0) {
-                    return '00';
-                } else {
-                    return theMinute.padZeroes(2);
-                }
-            }
-
-            var theMinute = WOPR.startTime.getFormattedMinute();
-
-            theMinute = _adjustForSatellite(theMinute);
-            theMinute = _adjustForZeroMinutes(theMinute);
-
-            return WOPR.startTime.getFullYear() + WOPR.startTime.getDayOfYear() +
-                '_' + WOPR.startTime.getFormattedHour() + theMinute;
-        }
-
-        this.getFrames = function() {
+        this.getFrames = function () {
             var tempArray = [];
-            var newTime;
+            var newTime = WOPR.startTime;
+            var i;
 
-            newTime = WOPR.startTime;
-
-            for (var i = 0; i < 15; i = i + 1) {
+            for (i = 0; i < 15; i = i + 1) {
                 tempArray.push(WOPR.getURI());
                 newTime.setUTCMinutes(newTime.getUTCMinutes() + 30);
                 WOPR.setStartTime(newTime);
@@ -112,17 +113,22 @@ goog.require('WOPR.helpers');
 
     };
 
-    WOPR.loadPage = function() {
+    WOPR.loadPage = function () {
         var startTime = new Date();
+        var item;
+
+        this.change = function () {
+            WOPR.loadPage();
+        };
 
         startTime.setUTCHours(startTime.getUTCHours() - 1);
 
         WOPR.setStartTime(startTime);
 
-        for (var item in WOPR.controls) {
-            WOPR.controls[item].addEventListener('change', function() {
-                WOPR.loadPage();
-            });
+        for (item in WOPR.controls) {
+            if (WOPR.controls.hasOwnProperty(item)) {
+                WOPR.controls[item].addEventListener('change', this.change.bind(this), false);
+            }
         }
 
         WOPR.loadImages();
@@ -131,9 +137,10 @@ goog.require('WOPR.helpers');
     /**
      * Create an array of image elements into the DOM.
      */
-    WOPR.loadImages = function() {
+    WOPR.loadImages = function () {
         var imgElement;
         var fArray = WOPR.getFrames();
+        var c;
 
         /** Remove any images that may have been loaded previously. */
         while (WOPR.display.firstChild) {
@@ -141,7 +148,7 @@ goog.require('WOPR.helpers');
         }
 
         /** Build image tags for each frame of the animated loop. Then inject each into the DOM. */
-        for (var c = 0; c < fArray.length; c++) {
+        for (c = 0; c < fArray.length; c = c + 1) {
 
             imgElement = document.createElement('img');
 
@@ -160,15 +167,15 @@ goog.require('WOPR.helpers');
      * @param {number} n
      */
 
-    WOPR.animateFrames = function(n) {
+    WOPR.animateFrames = function (n) {
 
         var theFrames = document.getElementsByClassName('frame');
 
-        if ((n == '') || (n == null) || (typeof n == null)) {
+        if ((!n) || (n === null)) {
             n = 0;
         }
 
-        setTimeout(function() {
+        setTimeout(function () {
 
             theFrames.item(n).classList.add('hidden');
 
